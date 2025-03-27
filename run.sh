631e041ebffe18ed5a9550d1d1dae14f05ba4aea
alexandr.safonichev@crmgroup.ru <alexandr.safonichev@crmgroup.ru>
2025-03-27 18:29:23 +0300

    1

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
 echo "Hello world!"
+echo "option beer \"${BEER}\";"
+echo "option wine \"${WINE}\";"
+echo "option liquor \"${LICOUR}\";"
+echo "option name \"${NAME}\";"
+echo "option year \"${YEAR}\";"
