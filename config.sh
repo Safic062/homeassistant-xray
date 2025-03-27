2ae00379c469db1a4cbf17123967e5f2446de7c0
alexandr.safonichev@crmgroup.ru <alexandr.safonichev@crmgroup.ru>
2025-03-27 22:19:38 +0300

    1

--- config.sh
+++ config.sh
@@ -38,74 +38,82 @@ EOF
 
 # echo "Результат: $result"
 
+for bridge in $(bashio::config 'bridges|keys'); do
+#while bashio::config "bridges.${index}.domain" > /dev/null; do
+  bashio::config.require.domain "bridges[${bridge}].domain"
+  bashio::config.require.local "bridges[${bridge}].local"
+  bashio::config.require.portal_address "bridges[${bridge}].portal_address"
+  bashio::config.require.portal_port "bridges[${bridge}].portal_port"
+  bashio::config.require.portal_user_id "bridges[${bridge}].portal_user_id"
+  bashio::config.require.in_tag "bridges[${bridge}].in_tag"
+  bashio::config.require.out_tag "bridges[${bridge}].out_tag"
 
-while bashio::config "bridges.${index}.domain" > /dev/null; do
-  DOMAIN=$(bashio::config "bridges.${index}.domain")
-  LOCAL=$(bashio::config "bridges.${index}.local")
-  PORTAL_ADDRESS=$(bashio::config "bridges.${index}.portal_address")
-  PORTAL_PORT=$(bashio::config "bridges.${index}.portal_port")
-  PORTAL_USER_ID=$(bashio::config "bridges.${index}.portal_user_id")
-  IN_TAG=$(bashio::config "bridges.${index}.in_tag")
-  OUT_TAG=$(bashio::config "bridges.${index}.out_tag")
+  DOMAIN=$(bashio::config "bridges.${bridge}.domain")
+  LOCAL=$(bashio::config "bridges.${bridge}.local")
+  PORTAL_ADDRESS=$(bashio::config "bridges.${bridge}.portal_address")
+  PORTAL_PORT=$(bashio::config "bridges.${bridge}.portal_port")
+  PORTAL_USER_ID=$(bashio::config "bridges.${bridge}.portal_user_id")
+  IN_TAG=$(bashio::config "bridges.${bridge}.in_tag")
+  OUT_TAG=$(bashio::config "bridges.${bridge}.out_tag")
 
   # Добавление элемента в массив bridges
   cat <<EOF >> /etc/xray/config.json
     {
       "tag": "$IN_TAG",
       "domain": "$DOMAIN"
-    }${next_comma}
+    }
 EOF
 
-  index=$((index + 1))
-  next_comma=","
+#   index=$((index + 1))
+#   next_comma=","
 done
 
-# Продолжение конфигурации
-cat <<EOF >> /etc/xray/config.json
-  ],
-  "outbound": [
-    {
-      "tag": "$OUT_TAG",
-      "protocol": "freedom",
-      "settings": {
-        "redirect": "$LOCAL"
-      }
-    },
-    {
-      "protocol": "vmess",
-      "settings": {
-        "vnext": [
-          {
-            "address": "$PORTAL_ADDRESS",
-            "port": $PORTAL_PORT,
-            "users": [
-              {
-                "id": "$PORTAL_USER_ID"
-              }
-            ]
-          }
-        ]
-      },
-      "tag": "interconn"
-    }
-  ],
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "inboundTag": ["$IN_TAG"],
-        "domain": ["full:$DOMAIN"],
-        "outboundTag": "interconn"
-      },
-      {
-        "type": "field",
-        "inboundTag": ["$IN_TAG"],
-        "outboundTag": "$OUT_TAG"
-      }
-    ]
-  }
-}
-EOF
+# # Продолжение конфигурации
+# cat <<EOF >> /etc/xray/config.json
+#   ],
+#   "outbound": [
+#     {
+#       "tag": "$OUT_TAG",
+#       "protocol": "freedom",
+#       "settings": {
+#         "redirect": "$LOCAL"
+#       }
+#     },
+#     {
+#       "protocol": "vmess",
+#       "settings": {
+#         "vnext": [
+#           {
+#             "address": "$PORTAL_ADDRESS",
+#             "port": $PORTAL_PORT,
+#             "users": [
+#               {
+#                 "id": "$PORTAL_USER_ID"
+#               }
+#             ]
+#           }
+#         ]
+#       },
+#       "tag": "interconn"
+#     }
+#   ],
+#   "routing": {
+#     "rules": [
+#       {
+#         "type": "field",
+#         "inboundTag": ["$IN_TAG"],
+#         "domain": ["full:$DOMAIN"],
+#         "outboundTag": "interconn"
+#       },
+#       {
+#         "type": "field",
+#         "inboundTag": ["$IN_TAG"],
+#         "outboundTag": "$OUT_TAG"
+#       }
+#     ]
+#   }
+# }
+# EOF
 
 echo "Конфигурация успешно сгенерирована: /etc/xray/config.json"
 
