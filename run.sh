ebbda01c341ae48119bce9e55278c3a76544d552
alexandr.safonichev@crmgroup.ru <alexandr.safonichev@crmgroup.ru>
2025-03-27 19:04:48 +0300

    1

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
 echo "Hello world!"
+
+BEER=$(bashio::config 'beer')
+WINE=$(bashio::config 'wine')
+LIQOUR=$(bashio::config 'liquor')
+NAME=$(bashio::config 'name')
+YEAR=$(bashio::config 'year')
+
 echo "option beer \"${BEER}\";"
 echo "option wine \"${WINE}\";"
 echo "option liquor \"${LIQOUR}\";"
